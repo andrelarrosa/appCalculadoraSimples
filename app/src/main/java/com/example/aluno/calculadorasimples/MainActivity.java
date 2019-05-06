2742cae5221de69ee0af53f1ce52e49476a2749e
Jonatas S. Oliveira <jnsopro@gmail.com>
2019-05-06 11:37:08 -0300

    decimal adicionado

--- app/src/main/java/com/example/aluno/calculadorasimples/MainActivity.java
+++ app/src/main/java/com/example/aluno/calculadorasimples/MainActivity.java
@@ -19,8 +19,20 @@ public class MainActivity extends AppCompatActivity {
     private Button btnVezes;
     private Button btnDiv;
     private Button btnIgual;
+    private Button btnNum1;
+    private Button btnNum2;
+    private Button btnNum3;
+    private Button btnNum4;
+    private Button btnNum5;
+    private Button btnNum6;
+    private Button btnNum7;
+    private Button btnNum8;
+    private Button btnNum9;
+    private Button btnNum0;
+    private Button btnDecimal;
     private double n1, n2;
     private char c;
+    private byte contDec = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +48,104 @@ public class MainActivity extends AppCompatActivity {
         btnMenos = (Button) findViewById(R.id.btnMenos);
         btnVezes = (Button) findViewById(R.id.btnVezes);
         btnIgual = (Button) findViewById(R.id.btnIgual);
+        btnNum1 = (Button) findViewById(R.id.btnNum1);
+        btnNum2 = (Button) findViewById(R.id.btnNum2);
+        btnNum3 = (Button) findViewById(R.id.btnNum3);
+        btnNum4 = (Button) findViewById(R.id.btnNum4);
+        btnNum5 = (Button) findViewById(R.id.btnNum5);
+        btnNum6 = (Button) findViewById(R.id.btnNum6);
+        btnNum7 = (Button) findViewById(R.id.btnNum7);
+        btnNum8 = (Button) findViewById(R.id.btnNum8);
+        btnNum9 = (Button) findViewById(R.id.btnNum9);
+        btnNum0 = (Button) findViewById(R.id.btnNum0);
+        btnDecimal = (Button) findViewById(R.id.btnDecimal);
 
-        //primeiro passo
+
+        btnNum1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "1");
+            }
+        });
+
+        btnNum2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "2");
+            }
+        });
+
+        btnNum3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "3");
+            }
+        });
+
+        btnNum4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "4");
+            }
+        });
+
+        btnNum5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "5");
+            }
+        });
+
+        btnNum6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "6");
+            }
+        });
+
+        btnNum7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "7");
+            }
+        });
+
+        btnNum8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "8");
+            }
+        });
+
+        btnNum9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "9");
+            }
+        });
+
+        btnNum0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cndNum.setText(cndNum.getText().toString() + "0");
+            }
+        });
+
+        btnDecimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (contDec <1){
+                    cndNum.setText(cndNum.getText().toString() + ",");
+                    contDec = 1;
+                }
+            }
+        });
+
         btnMais.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    n1 = Double.parseDouble(cndNum.getText().toString());
+                    n1 = Double.parseDouble(cndNum.getText().toString().replace(',','.'));
                     cndNum.setText(cndNum.getText().toString() + " + ");
                     c = '+';
                     bloquearOperacoes();
@@ -58,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    n1 = Double.parseDouble(cndNum.getText().toString());
+                    n1 = Double.parseDouble(cndNum.getText().toString().replace(',','.'));
                     cndNum.setText(cndNum.getText().toString() + " / ");
                     c = '/';
                     bloquearOperacoes();
@@ -74,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    n1 = Double.parseDouble(cndNum.getText().toString());
+                    n1 = Double.parseDouble(cndNum.getText().toString().replace(',','.'));
                     cndNum.setText(cndNum.getText().toString() + " - ");
                     c = '-';
                     bloquearOperacoes();
@@ -90,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    n1 = Double.parseDouble(cndNum.getText().toString());
+                    n1 = Double.parseDouble(cndNum.getText().toString().replace(',','.'));
                     cndNum.setText(cndNum.getText().toString() + " X ");
                     c = 'X';
                     bloquearOperacoes();
@@ -109,7 +212,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String aux = cndNum.getText().toString();
                     String a[] = aux.split(" ");
-                    n2 = Double.parseDouble(a[2]);
+                    n2 = Double.parseDouble(a[2].replace(',','.'));
 
                     if (c == '+') {
                         txtRes.setText("" + (n1 + n2));
@@ -138,6 +241,7 @@ public class MainActivity extends AppCompatActivity {
                 btnDiv.setEnabled(true);
                 btnVezes.setEnabled(true);
                 btnMais.setEnabled(true);
+                contDec = 0;
             }
         });
 
@@ -149,6 +253,7 @@ public class MainActivity extends AppCompatActivity {
         btnDiv.setEnabled(false);
         btnMais.setEnabled(false);
         btnVezes.setEnabled(false);
+        contDec = 0;
     }
 
 }
